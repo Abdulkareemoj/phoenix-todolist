181528d3ca37fa5714c44ad93735609197e1155b
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-03-07 10:44:44 +0000

    need to rethink this

--- react_todo_list/lib/react_todo_list_web/controllers/page_controller.ex
+++ react_todo_list/lib/react_todo_list_web/controllers/page_controller.ex
@@ -1,7 +1,14 @@
-defmodule ReactTodoListWeb.PageController do
-  use ReactTodoListWeb, :controller
+  # defmodule ReactTodoListWeb.PageController do
+  #   use ReactTodoListWeb, :controller
 
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
+  #   def index(conn, _params) do
+  #     render(conn, "index.html")
+  #   end
+  # end
+
+use ReactTodoListWeb, :controller
+alias ReactTodoList.Todo
+def index(conn, _params) do
+  tasks = Todo.list_tasks()
+  render(conn, "index.html", props: Poison.encode!(%{tasks: tasks}))
 end
